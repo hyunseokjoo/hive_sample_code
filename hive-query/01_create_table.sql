5c8f1be8fab2c8c3c7399bd1e6d2d9c3190c915c
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-10-05 21:29:31 +0900

    init: initail commit

---
+++ hive-query/01_create_table.sql
@@ -0,0 +1,49 @@
+-- 기본 메뉴얼
+CREATE [TEMPORARY] [EXTERNAL] TABLE [IF NOT EXISTS] [db_name.]table_name    -- (Note: TEMPORARY available in Hive 0.14.0 and later)
+  [(col_name data_type [column_constraint_specification] [COMMENT col_comment], ... [constraint_specification])]
+  [COMMENT table_comment]
+  [PARTITIONED BY (col_name data_type [COMMENT col_comment], ...)]
+  [CLUSTERED BY (col_name, col_name, ...) [SORTED BY (col_name [ASC|DESC], ...)] INTO num_buckets BUCKETS]
+  [SKEWED BY (col_name, col_name, ...)                  -- (Note: Available in Hive 0.10.0 and later)]
+     ON ((col_value, col_value, ...), (col_value, col_value, ...), ...)
+     [STORED AS DIRECTORIES]
+  [
+   [ROW FORMAT row_format] 
+   [STORED AS file_format]
+     | STORED BY 'storage.handler.class.name' [WITH SERDEPROPERTIES (...)]  -- (Note: Available in Hive 0.6.0 and later)
+  ]
+  [LOCATION hdfs_path]
+  [TBLPROPERTIES (property_name=property_value, ...)]   -- (Note: Available in Hive 0.6.0 and later)
+  [AS select_statement];   -- (Note: Available in Hive 0.5.0 and later; not supported for external tables)
+ 
+
+-- create managed or internal table 예제
+CREATE TABLE default.person(
+    pid     INT,
+    name    STRING,
+    age     INT,
+    address STRING
+)
+STORED AS ORC
+
+
+-- CREATE TABLE WITH LOCATION-HDFS
+CREATE TABLE default.person(
+    pid     INT,
+    name    STRING,
+    age     INT,
+    address STRING
+)
+STORED AS ORC
+LOCATION 'hdfs://{bucket_name}/{path}';
+
+
+-- CREATE TABLE WITH LOCATION-AWS S3
+CREATE TABLE default.person(
+    pid     INT,
+    name    STRING,
+    age     INT,
+    address STRING
+)
+STORED AS ORC
+LOCATION 's3a://{bucket_name}/{path}';
