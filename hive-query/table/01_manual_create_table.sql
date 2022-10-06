50ebaa9c41f304da5dbf4a9e2e1a340d997beb59
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2022-10-06 20:54:48 +0900

    fix : 코드 변경

--- hive-query/table/01_manual_create_table.sql
+++ hive-query/table/01_manual_create_table.sql
@@ -15,13 +15,7 @@ CREATE [TEMPORARY] [EXTERNAL] TABLE [IF NOT EXISTS] [db_name.]table_name    -- (
   [LOCATION hdfs_path]
   [TBLPROPERTIES (property_name=property_value, ...)]   -- (Note: Available in Hive 0.6.0 and later)
   [AS select_statement];   -- (Note: Available in Hive 0.5.0 and later; not supported for external tables)
-
-
-
-
-
-
-
+--------------------------------------
 
 -- CREATE TABLE WITH parquet
 -- parquet 파일은 꼭 Schema의 datatype을 맞춰야 한다.
